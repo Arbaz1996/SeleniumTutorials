ec278d3a66f4ee7186e5ad7c188f31117f2bd145
Muhammad Atif <atif.eccountant2@gmail.com>
2022-08-11 13:03:35 +0500

    2nd

---
+++ src/SohailSagar.java
@@ -0,0 +1,10 @@
+
+public class SohailSagar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Sohail Sagar is Great Person but Sometimes ");
+
+	}
+
+}
